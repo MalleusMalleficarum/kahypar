2d2de8be95b8d4ee84360e03b8e2d3aae27567ee
MalleusMalleficarum <dahonk1995>
2017-04-03 18:34:10 +0200

    release folder added

---
+++ release/tests/partition/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/andre/server-home/myHyparHypar/kahypar/tests/partition
+# Build directory: /home/andre/server-home/myHyparHypar/kahypar/release/tests/partition
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(partitioner_test "partitioner_test")
+add_test(metrics_test "metrics_test")
---
+++ release/tools/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/andre/server-home/myHyparHypar/kahypar/tools
+# Build directory: /home/andre/server-home/myHyparHypar/kahypar/release/tools
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(bookshelf_to_hgr_converter_test "bookshelf_to_hgr_converter_test")
+add_test(mtx_to_hgr_converter_test "mtx_to_hgr_converter_test")
+add_test(cnf_to_hgr_converter_test "cnf_to_hgr_converter_test")
+add_test(hgr_to_edge_list_conversion_test "hgr_to_edge_list_conversion_test")
