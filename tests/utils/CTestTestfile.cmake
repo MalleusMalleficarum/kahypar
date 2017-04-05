c89a2e77e8f5a926ce56949c0edfeffa0f219e08
MalleusMalleficarum <dahonk1995@web.de>
2017-04-05 16:35:52 +0200

    Removed a lot of wrong cmake stuff

--- tests/utils/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andre/server-home/myHyparHypar/kahypar/tests/utils
-# Build directory: /home/andre/server-home/myHyparHypar/kahypar/tests/utils
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(math_test "math_test")
